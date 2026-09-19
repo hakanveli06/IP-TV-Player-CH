137400ac652e8f52ee21cb04aacb0e3ad46e2599
hakan <hakan@ROOT-MOBIL>
2026-09-19 23:11:12 +0300

    Connect public v2 to TMDb proxy

--- js/runtime-config.js
+++ js/runtime-config.js
@@ -1,6 +1,6 @@
 /* Public v2 calisma ayarlari. Bu dosya yalnizca gizli olmayan adresleri
    icerir; TMDb kimlik bilgisi Cloudflare Worker secret olarak tutulur. */
 window.HM_RUNTIME = window.HM_RUNTIME || {
-  tmdbProxyBase: '',
+  tmdbProxyBase: 'https://hm-player-tmdb-proxy.hakanveli.workers.dev/tmdb/3',
   buildChannel: 'public-v2'
 };
